8dbf1b1a5d00126bc307f541c6bbbc29608467d6
Valentin Kornukh <valentino.net@gmail.com>
2021-05-24 23:57:33 -0500

    contact form email send added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,17 @@
-!(function($) {
+!(function ($) {
   "use strict";
 
   // Preloader
-  $(window).on('load', function() {
+  $(window).on('load', function () {
     if ($('#preloader').length) {
-      $('#preloader').delay(100).fadeOut('slow', function() {
+      $('#preloader').delay(100).fadeOut('slow', function () {
         $(this).remove();
       });
     }
   });
 
   // Smooth scroll for the navigation menu and links with .scrollto classes
-  $(document).on('click', '.nav-menu a, .mobile-nav a, .scrollto', function(e) {
+  $(document).on('click', '.nav-menu a, .mobile-nav a, .scrollto', function (e) {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       if (target.length) {
@@ -60,19 +60,19 @@
     $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button>');
     $('body').append('<div class="mobile-nav-overly"></div>');
 
-    $(document).on('click', '.mobile-nav-toggle', function(e) {
+    $(document).on('click', '.mobile-nav-toggle', function (e) {
       $('body').toggleClass('mobile-nav-active');
       $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
       $('.mobile-nav-overly').toggle();
     });
 
-    $(document).on('click', '.mobile-nav .drop-down > a', function(e) {
+    $(document).on('click', '.mobile-nav .drop-down > a', function (e) {
       e.preventDefault();
       $(this).next().slideToggle(300);
       $(this).parent().toggleClass('active');
     });
 
-    $(document).click(function(e) {
+    $(document).click(function (e) {
       var container = $(".mobile-nav, .mobile-nav-toggle");
       if (!container.is(e.target) && container.has(e.target).length === 0) {
         if ($('body').hasClass('mobile-nav-active')) {
@@ -87,7 +87,7 @@
   }
 
   // Back to top button
-  $(window).scroll(function() {
+  $(window).scroll(function () {
     if ($(this).scrollTop() > 100) {
       $('.back-to-top').fadeIn('slow');
     } else {
@@ -95,7 +95,7 @@
     }
   });
 
-  $('.back-to-top').click(function() {
+  $('.back-to-top').click(function () {
     $('html, body').animate({
       scrollTop: 0
     }, 1500, 'easeInOutExpo');
@@ -103,8 +103,8 @@
   });
 
   // Skills section
-  $('.skills-content').waypoint(function() {
-    $('.progress .progress-bar').each(function() {
+  $('.skills-content').waypoint(function () {
+    $('.progress .progress-bar').each(function () {
       $(this).css("width", $(this).attr("aria-valuenow") + '%');
     });
   }, {
@@ -126,12 +126,12 @@
   });
 
   // Porfolio isotope and filter
-  $(window).on('load', function() {
+  $(window).on('load', function () {
     var portfolioIsotope = $('.portfolio-container').isotope({
       itemSelector: '.portfolio-item'
     });
 
-    $('#portfolio-flters li').on('click', function() {
+    $('#portfolio-flters li').on('click', function () {
       $("#portfolio-flters li").removeClass('filter-active');
       $(this).addClass('filter-active');
 
@@ -142,7 +142,7 @@
     });
 
     // Initiate venobox (lightbox feature used in portofilo)
-    $(document).ready(function() {
+    $(document).ready(function () {
       $('.venobox').venobox({
         'share': false
       });
@@ -164,8 +164,55 @@
       once: true
     });
   }
-  $(window).on('load', function() {
+  $(window).on('load', function () {
     aos_init();
   });
+
+  // contact form
+
+  class ContactForm {
+    #receiver
+    #secureToken
+
+    constructor(receiver, secureToken) {
+      this.#receiver = receiver;
+      this.#secureToken = secureToken;
+    }
+
+    send(from, subject, body, callback) {
+      Email.send({
+        SecureToken: this.#secureToken,
+        To: this.#receiver,
+        From: from,
+        Subject: subject,
+        Body: body
+      }).then(callback);
+    }
+  }
+
+  $('#sendMessage').on("click", function () {
+
+    $(".error-message").hide();
+    $(".loading").show();
+
+    const contactForm = new ContactForm("e.kornukh@gmail.com", "bb759b9f-0fa4-4ca5-b1e5-b35e7843f9f6");
+
+    let from = $("#email").val();
+    let subject = $("#subject").val();
+    let message = "Message from " + $("#name").val() + ": <br/>" + $("#message").val();
+
+    contactForm.send(from, subject, message, callback);
+  })
+
+  function callback(message) {
+    $(".loading").hide();
+
+    if (message == "OK") {
+      $(".form-control").val("");
+      $(".sent-message").show(0).delay(4000).hide(0);
+    }
+    else
+      $(".error-message").show();
+  }
 
 })(jQuery);
